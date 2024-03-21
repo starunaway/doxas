20a28e87324bce9f877f21aab9099d415bb40418
mac <hao.li@xbotspace.com>
2024-03-21 14:30:30 +0800

    feat: support medium img

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,25 @@
-import DefaultTheme from 'vitepress/theme'
-import './custom.css'
+import DefaultTheme from 'vitepress/theme';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
 
-export default DefaultTheme
+import './custom.css';
+
+export default {
+  ...DefaultTheme,
+
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
+};
